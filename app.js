d62f641499c85fcc1e5bef73d48455cc8e235c40
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2022-11-10 01:53:55 -0300

    tablas con datos meteorologicos

--- app.js
+++ app.js
@@ -27,30 +27,29 @@ function rellenarDatos(){
 //     });
 // }
 
-const botonClima = document.getElementById('meteorologico');
-botonClima.onclick = function (){
-        fetch('https://api.weatherbit.io/v2.0/forecast/daily?lat=-26.8528&lon=-65.7083&days=10&lang=es&key=66a5873f866745edb4f9132616310ae5')
-        .then(response => response.json())
-        .then(data => mostrarClima(data));
-}
+// const botonClima = document.getElementById('temperaturaInfo');
+// botonClima.onclick = function (){
+//         fetch('https://api.weatherbit.io/v2.0/forecast/daily?lat=-26.8528&lon=-65.7083&days=10&lang=es&key=66a5873f866745edb4f9132616310ae5')
+//         .then(response => response.json())
+//         .then(data => mostrarClima(data));
+// }
 
-function mostrarClima(data) {
-    const datosClima = JSON.parse(JSON.stringify(data));
-    //window.location.href = './datosMeteorologicos.html';
+// function mostrarClima(data) {
+//     const datosClima = JSON.parse(JSON.stringify(data));
 
-    for(let i = 0; i < datosClima.data.length; i++) {
-        const cuerpoTabla = document.getElementById('cuerpoTabla1');    
-        const fila = document.createElement('tr');
-        const td1 = document.createElement('td');
-        td1.text = datosClima.data[i].datetime;
-        const td2 = document.createElement('td');
-        td2.text = datosClima.data[i].max_temp;
-        const td3 = document.createElement('td');
-        td3.text = datosClima.data[i].min_temp;
-        cuerpoTabla.appendChild(fila.appendChild(td1));
-        cuerpoTabla.appendChild(fila.appendChild(td2));
-        cuerpoTabla.appendChild(fila.appendChild(td3));
-        console.log(fila);
-        console.log(cuerpoTabla);
-    }
-}
+//     for(let i = 0; i < datosClima.data.length; i++) {
+//         const cuerpoTabla = document.getElementById('cuerpoTabla3');    
+//         const fila = document.createElement('tr');
+//         const td1 = document.createElement('td');
+//         td1.text = datosClima.data[i].datetime;
+//         const td2 = document.createElement('td');
+//         td2.text = datosClima.data[i].max_temp;
+//         const td3 = document.createElement('td');
+//         td3.text = datosClima.data[i].min_temp;
+//         cuerpoTabla.appendChild(fila.appendChild(td1));
+//         cuerpoTabla.appendChild(fila.appendChild(td2));
+//         cuerpoTabla.appendChild(fila.appendChild(td3));
+//         console.log(fila);
+//         console.log(cuerpoTabla);
+//     }
+// }
